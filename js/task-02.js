8e9a1221c5284b5c0207b39772bf42a7dbda9463
IvanPanin675 <107686584+IvanPanin675@users.noreply.github.com>
2022-11-02 10:24:47 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const storageEll = [];
+const listEl = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const itemEL = document.createElement("li");
+  itemEL.classList.add("item");
+  itemEL.textContent = ingredient;
+  storageEll.push(itemEL);
+});
+listEl.append(...storageEll);
